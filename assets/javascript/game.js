f01a4c67d1c636e2f384e73bd6f8b074d71f6861
reggiegfl <reggiegfl@yahoo.com>
2018-06-02 22:47:33 -0400

    Finished HW

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,44 +8,52 @@ var losses = 0;
 var guessesLeft = 9;
 var alreadyGuessed =[];
 
-var guess = null;
+
 var randomIndex = Math.floor(Math.random() * computerLetter.length);
-console.log(randomIndex);
+    console.log(randomIndex);
 var letter = computerLetter[randomIndex];
-console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + alreadyGuessed + " Computer picked: " + letter);
+    console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + alreadyGuessed + " Computer picked: " + letter);
 
 document.onkeyup = function(event) {
     var guess = String.fromCharCode(event.keyCode).toLowerCase();
-    document.getElementById("guessed").textContent = alreadyGuessed;
-    console.log(guess);
+    // document.getElementById("guessed").textContent = alreadyGuessed;
+        console.log(guess);
 
     // greater than 0 means it's a valid guess (not a puncruation mark or something)
-    if (alreadyGuessed.indexOf(guess) < 0) { 
-        
+    if (computerLetter.indexOf(guess) >= 0) { 
+            console.log(computerLetter.indexOf(guess));
+        alreadyGuessed.push(guess);
+        document.getElementById("guessed").textContent = alreadyGuessed;
+            console.log(alreadyGuessed);
+
         //win condition
         if (guess == letter) { 
-            alert("You won!");
+            //alert("You won!");
             wins++;
             document.getElementById("win").textContent = wins;
             console.log("Wins: " + wins);
+            resetGame ();
         } 
     // Doesn't match reduce guesses remaining
     // Counts down one on each wrong guess
-        else (guess != letter) 
-            alert("Guess again");
-            guessesLeft-1;
+        else {
+            //alert("Guess again");
+            guessesLeft--;
             document.getElementById("remaining").textContent = guessesLeft;
             console.log("remaining: " + guessesLeft);
-
+            // Counter reaches zero, game result is a loss
+                if (guessesLeft == 0) {
+                //alert("Game over");
+                losses++;
+                document.getElementById("lose").textContent = losses;    
+                console.log("lose: " + losses)
+                resetGame ();
+                }
+        }
+    }
         
 
-    // Counter reaches zero, game result is a loss
-        if (guessesLeft == 0)
-            alert("Game over");
-            losses++;
-            document.getElementById("lose").textContent = losses;    
-            console.log("lose: " + losses)
-        }
+    
 
         // display letters already guessed
 
@@ -53,7 +61,17 @@ document.onkeyup = function(event) {
         
         computerLetter[computerLetter.length]=guess;
 		// if it is a new letter then decrease remaining guesses by 1
-		guessesLeft--;
-    }
 
+        //guessesLeft--;
+}
+function resetGame (){
+    guessesLeft = 9;
+    alreadyGuessed = [];
+    randomIndex = Math.floor(Math.random() * computerLetter.length);
+        console.log(randomIndex);
+    letter = computerLetter[randomIndex];
+        console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + guessesLeft + " Guesses so far: " + alreadyGuessed + " Computer picked: " + letter);
+
+
+}
 
